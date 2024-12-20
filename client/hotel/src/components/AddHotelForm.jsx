533b732d9bf929a13da16ceb32bb64af1d6626f7
Rajvardhan <rajvardhanranawat80@gmail.com>
2024-12-20 13:18:29 +0530

    refactor AddHotelForm to integrate react-hook-form for improved form validation and submission handling; update Input component to support ref forwarding

--- client/hotel/src/components/AddHotelForm.jsx
+++ client/hotel/src/components/AddHotelForm.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import InputText from './Input'
 import Button from './Button'
+import {useForm} from 'react-hook-form'
 
 function AddHotelForm({name}) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
+  async function onSubmit(data) {
+    console.log(data)
+  }
   return (
     <>
     <div className=" shadow-md bg-gradient-to-br from-quaternary to-tertiary p-4 rounded-md">
@@ -10,10 +20,25 @@ function AddHotelForm({name}) {
           <h4>{name}</h4>
         </div>
         <div className="px-7 w-full lg:px-28  md:px-16  ">
-          <form className="grid  grid-cols-1 md:grid-cols-2 gap-y-4 gap-x-3">
-            <InputText />
-            <InputText />
-            <InputText className={'md:col-span-2'}/>
+          <form className="grid  grid-cols-1 md:grid-cols-2 gap-y-4 gap-x-3" 
+
+          
+          onSubmit={handleSubmit(onSubmit)}>
+          
+            <InputText 
+            
+            placeholder="Hotel Name"
+            {...register('hotelName', { required: 'Hotel Name is required' , minLength: { value: 3, message: 'Hotel Name should be at least 3 characters' } })}
+            />
+            <InputText 
+            
+            placeholder="address"
+            {...register('address', { required: 'Address is required' , minLength: { value: 3, message: 'Address should be at least 3 characters' } })}
+            />
+            <InputText type='file' className={'md:col-span-2'}
+            
+            {...register('image', { required: 'Image is required' })}
+            />
             <div className="m-auto md:col-span-2">
               <Button>Submit</Button>
             </div>
--- client/hotel/src/components/Input.jsx
+++ client/hotel/src/components/Input.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
+import {forwardRef} from 'react'
 
-function InputText({className,placeholder,type = "text",options=[],disabled=false}) {
+function InputText({className,placeholder,type = "text",options=[],disabled=false,...props},ref) {
   return (
     <>
       {type === 'option' ? (
-        <select disabled={disabled} className={` ${className} h-7 px-2 outline-none rounded-md bg-quinary`}>
+        <select {...props} ref={ref} disabled={disabled} className={` ${className} h-7 px-2 outline-none rounded-md bg-quinary`}>
           {options.map((option, index) => (
             <option key={index}>{option}</option>
           ))}
         </select>
       ) : (
-        <input disabled={disabled} type={type} placeholder={placeholder} className={` ${className} h-7 px-2 outline-none rounded-md bg-quinary`} />
+        <input {...props} ref={ref} disabled={disabled} type={type} placeholder={placeholder} className={` ${className} h-7 px-2 outline-none rounded-md bg-quinary`} />
       )}
     </>
   )
 }
 
-export default InputText
+export default  forwardRef(InputText)
