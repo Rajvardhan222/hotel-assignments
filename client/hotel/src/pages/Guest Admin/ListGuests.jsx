f7f9d3733d7a2fc03496208287fc87191805ac13
Rajvardhan <rajvardhanranawat80@gmail.com>
2024-12-20 09:40:08 +0530

    guest list demo page

--- client/hotel/src/pages/Guest Admin/ListGuests.jsx
+++ client/hotel/src/pages/Guest Admin/ListGuests.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import Table from '../../components/Table'
+const data = [
+  { name: 'John', age: 28, job: 'Developer' },
+  { name: 'Jane', age: 25, job: 'Designer' },
+];
 
+const columns = [
+  {
+    accessorKey: 'name', 
+    header: 'Name',
+  },
+  {
+    accessorKey: 'age',
+    header: 'Age',
+  },
+  {
+    accessorKey: 'job',
+    header: 'Job Title',
+  },
+];
 function ListGuests() {
   return (
-    <div>ListGuests</div>
+    <div className=''>
+      <div className='sm:p-2 md:p-8 lg:p-10'>
+        <h1 className='text-4xl text-center '>List of Guests</h1>
+      </div>
+      <div className='w-full overflow-x-scroll sm:p-3 lg:p-18'>
+        <Table data={data} columns={columns} />
+      </div>
+    </div>
   )
 }
 
