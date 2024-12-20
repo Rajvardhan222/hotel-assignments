74422ee8dbced42160481e16cff7cdf763be8ab9
Rajvardhan <rajvardhanranawat80@gmail.com>
2024-12-20 13:35:37 +0530

    refactor LoginForm to integrate react-hook-form for enhanced form validation and submission; add username and password fields with validation rules

--- client/hotel/src/components/LoginForm.jsx
+++ client/hotel/src/components/LoginForm.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import InputText from "./Input";
 import Button from "./Button";
+import {useForm} from 'react-hook-form'
 
 function LoginForm({ name }) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    
+  } = useForm()
+  async function login(data) {
+    console.log(data)
+    
+  }
   return (
     <>
       <div className=" shadow-md bg-gradient-to-br from-quaternary to-tertiary p-4 rounded-md">
@@ -10,9 +21,18 @@ function LoginForm({ name }) {
           <h4>{name}</h4>
         </div>
         <div className="px-7 w-full lg:px-28  md:px-16  ">
-          <form className="grid  grid-cols-1 md:grid-cols-2 gap-y-4 gap-x-3">
-            <InputText />
-            <InputText />
+          <form className="grid  grid-cols-1 md:grid-cols-2 gap-y-4 gap-x-3"  
+          
+          onSubmit={handleSubmit(login)}
+          >
+          
+            <InputText placeholer={'username'}
+            {...register('username', { required: 'Username is required' , minLength: { value: 3, message: 'Username should be at least 3 characters' } })}
+            />
+            <InputText type={'password'}  placeholer={'password'}
+            
+            {...register('password', { required: 'Password is required' , minLength: { value: 6, message: 'Password should be at least 6 characters' } })}
+            />
             <div className="m-auto md:col-span-2">
               <Button>Submit</Button>
             </div>
